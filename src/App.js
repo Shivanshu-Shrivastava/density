be96b8a6931e09669892a12e2293ca991c3b308f
Shivanshu Shrivastava <ssshrivastava2@gmail.com>
2023-07-14 20:38:49 +0530

    First Commit and Completed Navber Section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Navigate, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Header from './components/Main/Header';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Header />}  />
+          
+
+          
+          
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
